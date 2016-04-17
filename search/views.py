3ce4a4947eba4008d7e15d4e66d4e5f531061a37
Swati Lodha <swatilodha27@gmail.com>
2016-04-17 21:27:35 +0530

    Implemented multiple search tags

--- search/views.py
+++ search/views.py
@@ -14,17 +14,31 @@ def home(request):
 	return response
 
 def search_tag(request):
+        
         if request.method == "POST":
+            
             tag = request.POST.get("tag")
-            url = base_url + "tags/" + tag + "/media/recent?access_token=260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a"
+            tag_list = tag.split(",")
+            tagname = tag_list[0]
+            url = base_url + "tags/" + tagname + "/media/recent?access_token=260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a"
             resp = requests.get(url)
-
+            
             json = resp.json()
-
             image_link = []
+            
             for data in json['data']:
-		image_link.append(data['images']['low_resolution']['url'])
-
+                present = True
+                all_tag = data['tags']
+                input_tags = tag_list[1:]
+                
+                for item in input_tags:
+                    if (item not in all_tag):
+                        present = False
+                        break
+                    
+                if present:        
+                    image_link.append(data['images']['low_resolution']['url'])
+        
             return render(request,'search.html',{'image_link':image_link})
         
         else:
