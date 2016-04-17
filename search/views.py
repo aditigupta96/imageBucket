2e859e508e1e7b1aeb39ea2c8567563446c420e7
Mayank Agrawal <agarwalm214@gmail.com>
2016-04-17 17:47:23 +0530

    made first view

--- search/views.py
+++ search/views.py
@@ -7,9 +7,12 @@ client_secret = "25995932a2534cf09f9be5a87be61001"
 base_url = "https://api.instagram.com/v1/"
 
 def search_tag(request):
-	resp = requests.get("""https://api.instagram.com/v1/tags/lnmiit/media/recent?access_token=
-					260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a""")
+	resp = requests.get("https://api.instagram.com/v1/tags/lnmiit/media/recent?access_token=260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a")
 
 	json = resp.json()
 
-	
+	image_link = []
+	for data in json['data']:
+		image_link.append(data['link'])
+
+	return render(request,'index.html',{'image_link':image_link})
