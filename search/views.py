6141e96910d1b081c8ea9c38ce621c8eb5af6834
Mayank Agrawal <agarwalm214@gmail.com>
2016-04-17 16:49:54 +0530

    starting app

---
+++ search/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+access_token = "260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a"
+client_secret = "25995932a2534cf09f9be5a87be61001"
+
+base_url = "https://api.instagram.com/v1/"
+
+def search_tag(request):
+	resp = requests.get("""https://api.instagram.com/v1/tags/lnmiit/media/recent?access_token=
+					260885964.45f1b46.21e98a2bae1a46dab31568d770197a0a""")
+
+	json = resp.json()
+
+	
